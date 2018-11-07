c1e2a7d286736d10bf4964a82259540a9d844afe
David <david.khayutin@gmail.com>
2018-11-07 00:10:32 +0000

    final commit

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,8 +7,9 @@ puts "----------"
 
 Store.create name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true
 Store.create name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
-Store.create name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false
+Store.create name: 'Gastown', annual_revenue: 190000, mens_apparel: false, womens_apparel: true
 
 storeCount = Store.count
 
 puts "There are #{storeCount} stores currently using Active Record"
+
--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,12 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: {greater_than: 0}
   validates_associated :employees
+  validate :apparel
+end
+
+
+def apparel
+  if (!mens_apparel) && (!womens_apparel)
+    errors.add(:mens_apparel, :womens_apparel)
+  end
 end
